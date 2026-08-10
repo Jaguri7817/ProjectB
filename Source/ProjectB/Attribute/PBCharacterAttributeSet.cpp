591be41a37e1c94a3cb64926e49f90083b36b8cd
임승훈 <ish7817@naver.com>
2026-08-10 22:39:00 +0900

    어트리뷰트셋 구현
    기본적인 어트리뷰트셋 구현

--- Source/ProjectB/Attribute/PBCharacterAttributeSet.cpp
+++ Source/ProjectB/Attribute/PBCharacterAttributeSet.cpp
@@ -3,7 +3,20 @@
 
 #include "Attribute/PBCharacterAttributeSet.h"
 
-UPBCharacterAttributeSet::UPBCharacterAttributeSet()
+UPBCharacterAttributeSet::UPBCharacterAttributeSet() :
+	AttackRange(100.0f),
+	MaxAttackRange(300.0f),
+	AttackRadius(50.f),
+	MaxAttackRadius(150.0f),
+	Attack(30.0f),
+	MaxAttack(100.0f),
+	MaxHP(100.0f),
+	Damage(0.0f)
+{
+	InitHP(GetMaxHP());
+}
+
+void UPBCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
 {
 
 }
--- Source/ProjectB/Attribute/PBCharacterAttributeSet.h
+++ Source/ProjectB/Attribute/PBCharacterAttributeSet.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "PBCharacterAttributeSet.generated.h"
 
 
@@ -12,6 +13,8 @@
 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDeadDelegate);
 
 /**
  * 
@@ -24,9 +27,50 @@ class PROJECTB_API UPBCharacterAttributeSet : public UAttributeSet
 public:
 	UPBCharacterAttributeSet();
 
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, AttackRange);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, MaxAttackRange);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, AttackRadius);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, MaxAttackRadius);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, Attack);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, MaxAttack);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, HP);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, MaxHP);
+	ATTRIBUTE_ACCESSORS(UPBCharacterAttributeSet, Damage);
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	//virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	//virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
+	//virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
 
 protected:
 	// Ω∫≈» º≥∞Ë
+	UPROPERTY(BlueprintReadOnly, Category="Stat", Meta= (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRange;
 	
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRange;
 	
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData AttackRadius;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttackRadius;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Attack;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxAttack;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData HP;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MaxHP;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stat", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData Damage;
+
+	uint8 bIsDead = false;
+
 };
